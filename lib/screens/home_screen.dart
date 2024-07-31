7f505d97227d69c38134def4cdf322c47cd7eec3
Denesh Anandathasan <denesh555@gmail.com>
2024-07-31 00:57:09 -0400

    Desiging Section 1 - GoodMorning

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        //Section 1 - Top
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [],
+              ),
+              Row(
+                children: [],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
