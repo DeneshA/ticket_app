9b5dc1a57e284f759b651a431fe6097ba6afbc78
Denesh Anandathasan <denesh555@gmail.com>
2024-08-03 22:29:01 -0400

    Creating app_layout_builder_widget

--- lib/base/widgets/ticket_view.dart
+++ lib/base/widgets/ticket_view.dart
@@ -1,44 +1,60 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/style/app_styles.dart';
+import 'package:ticket_app/base/widgets/big_dot.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     // Find the screen size
     final size = MediaQuery.of(context).size;
 
     return SizedBox(
-      width: size.width*0.85,
+      width: size.width * 0.85,
       height: 179,
       child: Container(
-        margin: const EdgeInsets.only(right:16),
+        margin: const EdgeInsets.only(right: 16),
         padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: AppStyles.ticketBlue,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(21), topRight: Radius.circular(21))),
         child: Column(
           children: [
             Row(
               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "NYC", style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
+                  "NYC",
+                  style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
                 ),
-                Expanded(child: Container())
+                Expanded(child: Container()),
+                BigDot(),
+                //Stack  widget uses for over lapping items
+                const Expanded(child: Stack(children: [
+                  //LayoutBuilder() - to get the width / available space between 2 widgets
+                  //Flex() - leave space between 2 widget / '--'
+                  //List.generate() - generate No. of widget/ '-'
+                  //SizeBox() - either use sizedBox or Container to display widget/'-'
+                    SizedBox(
+                      child: Text("--------------------"),
+                    ),
+                  Center(child: Text("Plane"),)
+                ]
+                )),
+                BigDot(),
+                Expanded(child: Container()),
                 Text(
-                  "NYC", style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
+                  "NYC",
+                  style: AppStyles.headLineStyle3.copyWith(color: Colors.white),
                 )
               ],
             )
           ],
         ),
-          decoration: BoxDecoration(
-            color: AppStyles.ticketBlue,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(21), topRight: Radius.circular(21))
       ),
-      )
-      ,
     );
   }
 }
