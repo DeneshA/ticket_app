95becfa591044feaa1d133f1948cde77e9370e7f
Denesh Anandathasan <denesh555@gmail.com>
2024-08-09 22:28:49 -0400

    find ticket button designed

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -2,25 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/style/app_styles.dart';
 import 'package:ticket_app/screens/search/widgets/app_text_icon.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
+import 'package:ticket_app/screens/search/widgets/find_tickets.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Need scaffold to color the body
+    return Scaffold(
+      // Need scaffold to color the body
       backgroundColor: AppStyles.bgColor,
-      body: ListView( //it help us to scroll the screen
+      body: ListView(
+        //it help us to scroll the screen
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         children: [
-          const SizedBox(height: 40,),
-          Text("What are\nyou looking for?",style: AppStyles.headLineStyle1.copyWith(fontSize: 35),),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            "What are\nyou looking for?",
+            style: AppStyles.headLineStyle1.copyWith(fontSize: 35),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
           const AppTicketTabs(),
           const SizedBox(height: 25),
-          const AppTextIcon(icon: Icons.flight_takeoff_rounded,text: "Departure"),
+          const AppTextIcon(
+              icon: Icons.flight_takeoff_rounded, text: "Departure"),
           const SizedBox(height: 20),
-          const AppTextIcon(icon: Icons.flight_land_rounded,text: "Arrival"),
+          const AppTextIcon(icon: Icons.flight_land_rounded, text: "Arrival"),
+          const SizedBox(height: 25),
+          const FindTickets(),
         ],
       ),
     );
