735b5d6e8b7c30c8c8c50d2cf61df5f512a9d282
Denesh Anandathasan <denesh555@gmail.com>
2024-08-11 00:55:10 -0400

    EOD - Show ticket divider implemented

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -5,6 +5,7 @@ import 'package:ticket_app/base/widgets/ticket_view.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
 
 import '../../base/utils/all_json.dart';
+import '../../base/widgets/app_colum_text_layout.dart';
 
 class TicketScreen extends StatelessWidget {
   const TicketScreen({super.key});
@@ -24,7 +25,34 @@ class TicketScreen extends StatelessWidget {
           Container(
             padding: const EdgeInsets.only(left: 16),
               child: TicketView(ticket: ticketList[0],isColor: true,)
-          )
+          ),
+          const SizedBox(height: 1,),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15), //to match the top potion of ticket
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            color: AppStyles.ticketColor,
+            child: const Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppColumnTextLayout(
+                      topText: "Flutter DB",
+                      bottomText: "Passenger",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    AppColumnTextLayout(
+                      topText: "5221 36486",
+                      bottomText: "Passport",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
