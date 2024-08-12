5d0f00498a2d33fb7353a870dc25f643f9230b6e
Denesh Anandathasan <denesh555@gmail.com>
2024-08-12 09:41:29 -0400

    Tickete screen design completed

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -25,6 +26,7 @@ class TicketScreen extends StatelessWidget {
           const SizedBox(height: 20,),
           const AppTicketTabs(firstTab: "Upcoming",secondTab: "Previous"),
           const SizedBox(height: 20,),
+          //White and black ticket
           Container(
             padding: const EdgeInsets.only(left: 16),
               child: TicketView(ticket: ticketList[0],isColor: true,)
@@ -56,7 +58,7 @@ class TicketScreen extends StatelessWidget {
                 SizedBox(height: 20),
                 const AppLayoutbuilderWidget(randomDivider: 15,width: 5,isColor: false),
                 const SizedBox(height: 20),
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     AppColumnTextLayout(
@@ -101,6 +103,39 @@ class TicketScreen extends StatelessWidget {
                 ),
               ],
             ),
+          ),
+          const SizedBox(height: 1,),
+          //Bottom of the ticket section
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            padding:  const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              color: AppStyles.ticketColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(21),
+                bottomLeft: Radius.circular(21)
+              )
+            ),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: BarcodeWidget(
+                  height: 70,
+                barcode: Barcode.code128(),
+                  data: 'https://www.dbestech.com',
+                  drawText: false,
+                  color: AppStyles.textColor,
+                  width: double.infinity,
+              ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20,),
+          //Colorful ticket
+          Container(
+              padding: const EdgeInsets.only(left: 16),
+              child: TicketView(ticket: ticketList[0])
           ),
         ],
       ),
