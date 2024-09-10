225553bef24cb308981d8eae79af5a606feff63a
Denesh Anandathasan <denesh555@gmail.com>
2024-09-10 15:16:48 -0400

    build and run riverpod_build for bottom_nav_provider

---
+++ lib/provider/bottom_nav_provider.dart
@@ -0,0 +1,10 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'bottom_nav_provider.g.dart';
+@riverpod
+class BottomNavBarNotifier extends _$BottomNavBarNotifier{
+
+  @override
+  int build(){
+    return 0;
+  }
+}
